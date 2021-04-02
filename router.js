f2ff639f2de1f74a54b4983ffb90735eec84841b
purisquiri <mau.dorigo@outlook.es>
2021-04-02 11:17:06 -0400

    creating routes with Navigo

---
+++ router.js
@@ -0,0 +1,27 @@
+import Stories from "./pages/stories.js";
+
+const router = new Navigo(null, true, "#");
+console.log(router);
+
+export default class RouterHandler {
+  constructor() {
+    this.createRoutes();
+  }
+
+  createRoutes() {
+    const routes = [
+      { path: "/", page: Stories },
+      { path: "/new", page: Stories },
+      { path: "/ask", page: Stories },
+      { path: "/show", page: Stories },
+    ];
+
+    routes.forEach((route) => {
+      router
+        .on(route.path, () => {
+          route.page(route.path);
+        })
+        .resolve();
+    });
+  }
+}
